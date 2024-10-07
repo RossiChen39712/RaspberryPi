8fbebde1b2e1b4a7f9d17e5030b42808981deaeb
Rossi Chen <a171421876@gmail.com>
2024-10-07 14:33:03 +0800

    Revert "update skd"
    This reverts commit e56fa33f77201f436af48cdf16904b31bc4ffcd3.

--- C/key.c
+++ C/key.c
@@ -33,37 +33,59 @@ int main()
     }
     board.fd = serial_fd;
 
-    // 初始化 GPIO
-    struct gpiod_chip *chip = gpio_init("gpiochip4");
+    // GPIO 設置
+    struct gpiod_chip *chip;
+    struct gpiod_line *key1, *key2;
+
+    // 設定 GPIO4 芯片中的第 13 和 23 引腳
+    chip = gpiod_chip_open_by_name("gpiochip4");
     if (!chip)
     {
+        perror("Failed to open GPIO chip");
         return -1;
     }
 
+    key1 = gpiod_chip_get_line(chip, 13); // key1 綁定 GPIO13
+    if (!key1)
+    {
+        perror("Failed to get key1 line");
+        gpiod_chip_close(chip);
+        return -1;
+    }
+    gpiod_line_request_input_flags(key1, "key1", GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP);
+
+    key2 = gpiod_chip_get_line(chip, 23); // key2 綁定 GPIO23
+    if (!key2)
+    {
+        perror("Failed to get key2 line");
+        gpiod_chip_close(chip);
+        return -1;
+    }
+    gpiod_line_request_input_flags(key2, "key2", GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP);
+
+    // 主循環，根據按鍵狀態設置 RGB 顏色
     while (start)
     {
-        // 讀取按鍵狀態
-        int key1_state = gpio_read_key(chip, 13);
-        int key2_state = gpio_read_key(chip, 23);
+        int key1_state = gpiod_line_get_value(key1);
+        int key2_state = gpiod_line_get_value(key2);
 
         if (key1_state == 0)
         {
-            // 設置 RGB 燈為紅色
+            // 按鍵1被按下，設置 RGB 為紅色
             RgbPixel red_pixels[2] = {{1, 255, 0, 0}, {2, 255, 0, 0}};
             board_set_rgb(&board, red_pixels, 2);
         }
 
         if (key2_state == 0)
         {
-            // 設置 RGB 燈為藍色
+            // 按鍵2被按下，設置 RGB 為藍色
             RgbPixel blue_pixels[2] = {{1, 0, 0, 255}, {2, 0, 0, 255}};
             board_set_rgb(&board, blue_pixels, 2);
         }
 
-        printf("\rkey1: %d key2: %d", key1_state, key2_state);
+        printf("\rkey1: %d key2: %d", key1_state, key2_state); // 打印按鍵狀態
         fflush(stdout);
-
-        usleep(1000); // 延遲 1 毫秒
+        usleep(1000); // 睡眠 1 毫秒
     }
 
     // 清理並關閉 GPIO
